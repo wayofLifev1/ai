d3611b1c380fae96a68ea182a563c8e13104c652
PipahCookies <rbiatul21@gmail.com>
2026-01-04 10:19:19 +0800

    Update env.js

--- env.js
+++ env.js
@@ -2,3 +2,4 @@ const KEYS = {
             GROQ:   "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",    // Get from console.groq.com
             HF:     "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", // Get from huggingface.co
             GEMINI: "YOUR_GEMINI_API_KEY"
+};
