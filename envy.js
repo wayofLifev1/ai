efded2333620862c7f1c1cabb2d9a57a923c2bbc
PipahCookies <rbiatul21@gmail.com>
2026-01-04 10:11:25 +0800

    Delete envy.js

--- envy.js
+++
@@ -1,4 +0,0 @@
-const KEYS = {
-            GROQ:   "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",    // Get from console.groq.com
-            HF:     "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", // Get from huggingface.co
-            GEMINI: "YOUR_GEMINI_API_KEY"
